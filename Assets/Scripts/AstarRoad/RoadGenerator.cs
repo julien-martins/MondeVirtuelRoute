fecc0d100a81db59d085bc61c1ee84a89b652a91
julien-martins <39356114+julien-martins@users.noreply.github.com>
2022-12-07 21:43:14 +0100

    add comment and make bezier curve

--- Assets/Scripts/AstarRoad/RoadGenerator.cs
+++ Assets/Scripts/AstarRoad/RoadGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using Curves;
 using UnityEngine;
@@ -37,25 +38,28 @@ public class Node
     }
 }
 
+[RequireComponent(typeof(Grid))]
 public class RoadGenerator : MonoBehaviour
-{ 
+{
+    [Header("Grid Parameter")]
     public Grid Grid;
     public int TileSize = 10;
+    public GameObject CasePrefab;
 
-    private Node[,] nodes;
-    private float[,] PerlinValue;
-
+    [Header("Astar Parameter")]
     public Vector2Int StartPoint;
     public Vector2Int EndPoint;
-
+    
+    [Header("Perlin Noise Parameter")]
     public float PerlinScale = 1.0f;
     [Range(0, 1.0f)]
     public float PerlinThreshold = 0.3f;
 
-    public GameObject CasePrefab;
+    public GameObject Test;
     
-    public int PathSplit = 4;
-
+    private Node[,] nodes;
+    private float[,] PerlinValue;
+    
     private bool init = false;
     
     private List<Node> path;
@@ -74,11 +78,10 @@ public class RoadGenerator : MonoBehaviour
     }
     
     private void OnDrawGizmos()
-    {
-        
+    {   
         DrawGrid();
-        
 
+        if (badNode == null) return;
         foreach (var node in badNode)
         {
             node.Color = Color.red;
@@ -92,14 +95,16 @@ public class RoadGenerator : MonoBehaviour
         foreach (var node in testClotho)
         {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawCube(new Vector3(node.x, 0, node.y), Vector3.one * TileSize / 2);
+            Gizmos.DrawCube(new Vector3(node.x, 0, node.y), Vector3.one * TileSize);
         }
+        
     }
-
+    
+    /*
+     * Generate a grid with perlin noise value inside
+     */
     public void GeneratePerlinNoise()
     {
-        Debug.Log("Generate Perlin Noise");
-        
         PerlinValue = new float[(int)Grid.cellSize.x, (int)Grid.cellSize.y];
 
         for (int j = 0; j < Grid.cellSize.y; ++j)
@@ -116,11 +121,11 @@ public class RoadGenerator : MonoBehaviour
         }
     }
     
-    //Initialize the grid with all nodes which they use in the Astar algorithm
+    /*
+     * Initialize the grid with all nodes which they use in the Astar algorithm
+     */
     public void InitializeGrid()
     {   
-        Debug.Log("Initialize the grid");
-        
         nodes = new Node[(int)Grid.cellSize.x, (int)Grid.cellSize.y];
         
         var offset_x = (Grid.cellSize.x * TileSize) / 2 + TileSize/2;
@@ -144,6 +149,9 @@ public class RoadGenerator : MonoBehaviour
         }
     }
 
+    /*
+     * Draw the grid only with gizmos for now
+     */
     void DrawGrid()
     {
         if (Application.isPlaying) return;
@@ -157,113 +165,111 @@ public class RoadGenerator : MonoBehaviour
                 //obj.transform.position = nodes[i, j].WorldPos;
            
                 Gizmos.color = nodes[i, j].Color;
-                Gizmos.DrawCube(nodes[i, j].WorldPos, Vector3.one * TileSize / 2);
+                Gizmos.DrawCube(nodes[i, j].WorldPos, Vector3.one * TileSize /2);
             }
         }
     }
     
-    int Heuristic(Node start, Node target)
-    {
-        var dx = target.Index.x - start.Index.x;
-        var dy = target.Index.y - start.Index.y;
-
-        return Math.Abs(dx) + Math.Abs(dy);
-    }
-
-    bool InMatrix(Vector2Int p)
-    {
-        if (p.x < 0 || p.x >= Grid.cellSize.x || p.y < 0 || p.y >= Grid.cellSize.y) return false;
-
-        return true;
-    }
-
-    private bool goalReach = false;
-
+    /*
+     * callback function called by the PathFindingEditor class
+     */
     public void FindPathAction()
     {
         testClotho.Clear();
         
+        GeneratePerlinNoise();
         InitializeGrid();
         FindPath(nodes[StartPoint.x, StartPoint.y], nodes[EndPoint.x, EndPoint.y]);
         path = RecoverPath(nodes[EndPoint.x, EndPoint.y]);
-        
-        badNode = GetStrongAngle(path);
 
+        badNode = SmoothPath(path);
+
+
+        /* TO DELETE
         for (int i = 0; i < badNode.Count-1; i += 2)
         {
             MakeClothoide(badNode[i+1].WorldPos, badNode[i].WorldPos);
         }
-        
+        */
+
     }
 
-    List<Node> GetNeightbors(Node n)
+    /* TO DELETE
+    public void MakeClothoide(Vector3 start, Vector3 end)
+    {
+        var dir = end - start;
+        _clotho = new Clothoid(start.x, start.z, -1.8, 0.0, 0.5, Vector3.Distance(start, end));
+        //_clotho =  Clothoid.FromPoseAndPoint(0.0, 0.0, 0.0, 25.0, 25.0);
+        Vector3 centre = new Vector3();
+
+        foreach (Point2D point in _clotho.GetPoints(5))
+        {
+            centre.x = (float)point.X;
+            centre.y = (float)point.Y;
+            
+            centre.z = 0.0f;
+
+            testClotho.Add(new Vector2Int((int)point.X, (int)point.Y));
+            
+            //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //go.transform.position = centre;
+            //go.transform.parent = transform;
+
+        }
+    }
+    */
+
+    #region SmoothProcedureFunction
+    
+    /*
+     * Smooth the path with bezier curves
+     */
+    public List<Node> SmoothPath(List<Node> path)
+    {
+        var badPos = GetStrongAngle(path);
+        var mergedNode = MergeErrorNode(path, badPos);
+        var splitErrorNodes = SplitErrorNode(path, mergedNode);
+
+        return GenerateSmoothCurves(splitErrorNodes);
+    }
+    
+    /*
+     * Generate curve with QuadraticBezierCurves function
+     * on list of nodes returned by SmoothPath
+     */
+    public List<Node> GenerateSmoothCurves(List<Node> errorNodes)
     {
         List<Node> result = new();
 
-        Vector2Int[] directions = { Vector2Int.up,  Vector2Int.down, Vector2Int.left, Vector2Int.right, new Vector2Int(1, 1), new Vector2Int(-1, 1), new Vector2Int(1, -1), new Vector2Int(-1, -1),  };
-
-        foreach (var dir in directions)
+        for (int i = 0; i < errorNodes.Count; i += 3)
         {
-            Vector2Int newPos = n.Index + dir;
+            Vector3 p0 = errorNodes[i].WorldPos;
+            Vector3 p1 = errorNodes[i + 1].WorldPos;
+            Vector3 p2 = errorNodes[i + 2].WorldPos;
+            
+            for (float t = 0; t <= 1; t += 0.1f)
+            {
+                Vector3 p = QuadraticBezierCurves(t, p0, p1, p2);
 
-            if (!InMatrix(newPos)) continue;
-
-            result.Add(nodes[newPos.x, newPos.y]);
+                var offsetX = (Grid.cellSize.x/2) * TileSize;
+                var offsetZ = (Grid.cellSize.y/2) * TileSize;
+                
+                Vector2Int coord = new Vector2Int((int)Mathf.Abs((p.x + offsetX) / TileSize ), (int)Mathf.Abs((p.z + offsetZ) / TileSize ));
+                
+                if (InMatrix(coord))
+                {
+                    result.Add(nodes[coord.x, coord.y]);
+                }
+            }
         }
         
         return result;
     }
-    
-    Node GetSmallestNode(List<Node> nodes)
-    {
-        Node min = nodes[0];
-        
-        for (int i = 1; i < nodes.Count; ++i)
-        {
-            if (nodes[i].Heuristique < min.Heuristique)
-                min = nodes[i];
-        }
-        
-        return min;
-    }
-    
-    void FindPath(Node start, Node end)
-    {
-        List<Node> closed = new();
-        List<Node> open = new();
 
-        open.Add(start);
-        
-        
-        while (open.Count > 0)
-        {
-            Node u = GetSmallestNode(open);
-            open.Remove(u);
-
-            if (u.Index == end.Index)
-            {
-                goalReach = true;
-                return;
-            }
-
-            List<Node> neightbors = GetNeightbors(u);
-            foreach (var neightbor in neightbors)
-            {
-                Node v = neightbor;
-                if (!closed.Contains(v) && !open.Contains(v) && PerlinValue[v.Index.x, v.Index.y] > PerlinThreshold)
-                {
-                    v.Cout = u.Cout + 1;
-                    v.Heuristique = v.Cout + Heuristic(v, end);
-                    v.Pred = u;
-                    open.Add(v);
-                }
-                
-                closed.Add(u);
-            }
-
-        }
-    }
-
+    /*
+     * Get the list of nodes where there are a big variation of angle in the path generated by the astar
+     * path: list of nodes generated by the astar function
+     */
     List<Node> GetStrongAngle(List<Node> path)
     {
         List<Node> BadPos = new();
@@ -293,7 +299,12 @@ public class RoadGenerator : MonoBehaviour
         
         return BadPos;
     }
-
+    
+    /*
+     * function use to merge all the error node on the path generated by GetStrongAngle function
+     * path: path generated by the astar
+     * badPos: list of nodes generated by GetStrongAngle
+     */
     List<Node> MergeErrorNode(List<Node> path, List<Node> badPos)
     {
         List<Node> mergedErrorNodes = new List<Node>();
@@ -324,6 +335,12 @@ public class RoadGenerator : MonoBehaviour
 
         return mergedErrorNodes;
     }
+    
+    /*
+     * Take the path with all error merged and create three point which will used on the bezier curves function
+     * path: path generated by the astar algorithm
+     * badPos: list of error nodes returned by the MergeErrorNode function
+     */
     List<Node> SplitErrorNode(List<Node> path, List<Node> badPos)
     {
         List<Node> splitErrorNodes = new();
@@ -342,6 +359,8 @@ public class RoadGenerator : MonoBehaviour
                     break;
                 }
                 
+                splitErrorNodes.Add(path[i]);
+                
                 for (int k = i + 5; k > i; k--)
                 {
                     if (k > path.Count) continue;
@@ -356,29 +375,58 @@ public class RoadGenerator : MonoBehaviour
         return splitErrorNodes;
     }
 
-    public void MakeClothoide(Vector3 start, Vector3 end)
+    /*
+     * Basic Quadratic Bezier function
+     * with 0 <= t <= 1, and three points p0, p1, p2
+     */
+    public Vector3 QuadraticBezierCurves(float t, Vector3 p0, Vector3 p1, Vector3 p2)
     {
-        var dir = end - start;
-        _clotho = new Clothoid(start.x, start.z, -1.8, 0.0, 0.5, Vector3.Distance(start, end));
-        //_clotho =  Clothoid.FromPoseAndPoint(0.0, 0.0, 0.0, 25.0, 25.0);
-        Vector3 centre = new Vector3();
+        return (1 - t)*((1-t)*p0 + t * p1) + t * ((1 - t) * p1 + t * p2);
+    }
+    #endregion
+    
+    #region Astar Algorithm
+    
+    /*
+     * Implementation of Astar algorithm with a perlin function as weight
+     */
+    void FindPath(Node start, Node end)
+    {
+        List<Node> closed = new();
+        List<Node> open = new();
 
-        foreach (Point2D point in _clotho.GetPoints(5))
+        open.Add(start);
+        
+        
+        while (open.Count > 0)
         {
-            centre.x = (float)point.X;
-            centre.y = (float)point.Y;
-            
-            centre.z = 0.0f;
+            Node u = GetSmallestNode(open);
+            open.Remove(u);
 
-            testClotho.Add(new Vector2Int((int)point.X, (int)point.Y));
-            
-            //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //go.transform.position = centre;
-            //go.transform.parent = transform;
+            if (u.Index == end.Index) return;
+
+            List<Node> neightbors = GetNeightbors(u);
+            foreach (var neightbor in neightbors)
+            {
+                Node v = neightbor;
+                if (!closed.Contains(v) && !open.Contains(v) && PerlinValue[v.Index.x, v.Index.y] > PerlinThreshold)
+                {
+                    v.Cout = u.Cout + 1;
+                    v.Heuristique = v.Cout + Heuristic(v, end);
+                    v.Pred = u;
+                    open.Add(v);
+                }
+                
+                closed.Add(u);
+            }
 
         }
     }
     
+    /*
+     * Recover the all path created by the astar algorithm
+     * n: the end node pass in the FindPath function
+     */
     List<Node> RecoverPath(Node n)
     {
         List<Node> result = new();
@@ -395,4 +443,64 @@ public class RoadGenerator : MonoBehaviour
         return result;
     }
     
+    /*
+     * Get all neightbors around a node
+     * used for the astar algorithm
+     */
+    List<Node> GetNeightbors(Node n)
+    {
+        List<Node> result = new();
+
+        Vector2Int[] directions = { Vector2Int.up,  Vector2Int.down, Vector2Int.left, Vector2Int.right, new Vector2Int(1, 1), new Vector2Int(-1, 1), new Vector2Int(1, -1), new Vector2Int(-1, -1),  };
+
+        foreach (var dir in directions)
+        {
+            Vector2Int newPos = n.Index + dir;
+
+            if (!InMatrix(newPos)) continue;
+
+            result.Add(nodes[newPos.x, newPos.y]);
+        }
+        
+        return result;
+    }
+    
+    /*
+     * Check if a point p is in the grid which is a component attach to the gameobject
+     */
+    bool InMatrix(Vector2Int p)
+    {
+        if (p.x < 0 || p.x >= Grid.cellSize.x || p.y < 0 || p.y >= Grid.cellSize.y) return false;
+
+        return true;
+    }
+    
+    /*
+     * Get the smallest node on the list
+     * used for the astar algorithm
+     */
+    Node GetSmallestNode(List<Node> nodes)
+    {
+        Node min = nodes[0];
+        
+        for (int i = 1; i < nodes.Count; ++i)
+        {
+            if (nodes[i].Heuristique < min.Heuristique)
+                min = nodes[i];
+        }
+        
+        return min;
+    }
+    
+    /*
+     * Heuristic function use to the astar algorithm
+     */
+    int Heuristic(Node start, Node target)
+    {
+        var dx = target.Index.x - start.Index.x;
+        var dy = target.Index.y - start.Index.y;
+
+        return Math.Abs(dx) + Math.Abs(dy);
+    }
+    #endregion
 }
